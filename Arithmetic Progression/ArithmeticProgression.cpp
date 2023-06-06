c8a90995c2d97d8e9b613d819e75dd2ed4d9ae04
Filip Sramek <sramek.filip@gmail.com>
2023-06-06 21:27:42 +0200

    Another idea

--- Arithmetic Progression/ArithmeticProgression.cpp
+++ Arithmetic Progression/ArithmeticProgression.cpp
@@ -1,15 +1,9 @@
 #include <vector>
 #include <iostream>
 #include <math.h>
-#include <algorithm>
-#include <numeric>
-#include <set>
 #include <vector>
-#include <iostream>
 #include <math.h>
 #include <algorithm>
-#include <numeric>
-#include <set>
 
 class Solution
 {
@@ -18,43 +12,19 @@ public:
     {
         std::sort(std::begin(arr), std::end(arr));
 
-        //std::set<int> set(arr.begin(), arr.end());
-
-       // int sum = std::accumulate(set.begin(), set.end(), 0);
-        //int sum1 = (static_cast<int>((arr.size() * (arr[0] + arr[arr.size() - 1]))) / 2);
-
-        //std::cout <<  / 2) << std::endl;
-        //std::cout << (static_cast<int>((arr.size() * (arr[0] + arr[arr.size() - 1]))) / 2) << std::endl;
-        //bool var = ((static_cast<int>((arr.size() * (arr[0] + arr[arr.size() - 1]))) / 2) == std::accumulate(set.begin(), set.end(), 0));
-
-       // if ((static_cast<int>((arr.size() * (arr[0] + arr[arr.size() - 1]))) / 2) == std::accumulate(set.begin(), set.end(), 0))
-        //{
-            
-            for (int i = 0; i < arr.size(); i ++)
-            {
-                std::cout << "index: " << i << std::endl;
-                if (i <= (arr.size() - 2))
-                {
-                    std::cout << "sum: " << abs(arr[i] - arr[i + 1]) << std::endl;
-                    std::cout << "sum1: " << abs(arr[(arr.size() - 1) - i] - arr[(arr.size() - 1) - i - 1]) << std::endl;
-
-                    
-                    if (abs(arr[i] - arr[i + 1]) != abs(arr[0] - arr[1]) || (abs(arr[(arr.size() - 1) - i] - arr[(arr.size() - 1) - i - 1]) != abs(arr[0] - arr[1])) )
-                    {
-                        return 0;
-                    }
-                    
-                }
-            }
-            return 1;
-        //}
-       // return 0;
+        for (int i = 2; i < arr.size(); i++)
+        {
+            if((arr[i] - arr [i - 1]) != arr[1] - arr[0])
+                return 0;
+        }
+        return 1;
     }
 };
 
 int main()
 {
-    std::vector<int> arr = {1,1,1,1};
+    //std::vector<int> arr = {1, 1, 1, 1};
+    std::vector<int> arr = {3,5,1};
 
     // std::vector<int> arr = {1,100};
     Solution SolutionObj;
