8dd194bcfd2d961716060030413eadda7a680819
Filip <filip.sramek@br-automation.com>
2023-07-17 12:42:33 +0200

    another solution

--- MergeSortedArray/MergeSortedArray.cpp
+++ MergeSortedArray/MergeSortedArray.cpp
@@ -1,36 +1,38 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <string.h>
+#include <cstring>
 
 class Solution
 {
 public:
-    void merge(std::vector<int> nums1, int m, std::vector<int> nums2, int n)
+    void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
     {
-        std::vector<int> result (nums1.size() + nums2.size());
-        std::merge(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), result.begin(), [&](int& a, int& b){
+        std::vector<int> result(nums1.size() + nums2.size());
+
+        std::merge(nums1.begin(), nums1.begin() + m, nums2.begin(), nums2.end(), result.begin(), [&](int &a, int &b)
+        {
             return a < b;
         });
 
-        nums1.clear();
-
-        for (int i = 0; i < result.size(); i++)
-        {
-            nums1.push_back(result[i]);
-        }
+        std::memcpy(&nums1[0], &result[0], sizeof(int) * nums1.size());
     }
 };
 
 int main()
 {
-    std::vector<int> nums1 {1,2,3};
-    std::vector<int> nums2 {2,5,6};
+    std::vector<int> nums1{1, 2, 3, 0, 0, 0};
+    std::vector<int> nums2{2, 5, 6};
     int m = 3;
     int n = 3;
 
     Solution SolutionObj;
     SolutionObj.merge(nums1, m, nums2, n);
-    int i = 0;
+
+    for (int i = 0; i < nums1.size(); i++)
+    {
+        std::cout << nums1[i] << " ";
+    }
+
     return 0;
 }
