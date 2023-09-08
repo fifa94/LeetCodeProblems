aaaf792be8cee6cdfff26bf8c5323544b69dcc33
Filip Šrámek <sramek.filip@gmail.com>
2023-09-08 17:36:08 +0200

    New problem

---
+++ PartitionArrayAccordingToGivenPivot/PartitionArrayAccordingToGivenPivot.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<vector>
+
+class Solution {
+public:
+    std::vector<int> pivotArray(std::vector<int>& nums, int pivot) {
+        
+    }
+};
+
+int main ()
+{
+    std::vector<int> nums {9,12,5,10,14,3,10};
+    int pivot = 10;
+    Solution SolutionObj;
+    SolutionObj.pivotArray(nums, pivot);
+    return 0;
+}
