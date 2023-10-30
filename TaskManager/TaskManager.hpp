878d813f308aa0bfe9dd20b49bff9c206ffe95d2
Filip Šrámek <sramek.filip@gmail.com>
2023-10-30 20:44:04 +0100

    Created TaskManager files

---
+++ TaskManager/TaskManager.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <functional>
+
+class Task
+{
+private:
+    std::string _task;
+    std::string _taskDescription;
+public:
+    Task(/* args */);
+    ~Task();
+};
+
+Task::Task(/* args */)
+{
+}
+
+Task::~Task()
+{
+}
+
+class TaskManager
+{
+private:
+    /* data */
+public:
+    TaskManager(/* args */);
+    ~TaskManager();
+};
+
+TaskManager::TaskManager(/* args */)
+{
+}
+
+TaskManager::~TaskManager()
+{
+}
+
+
