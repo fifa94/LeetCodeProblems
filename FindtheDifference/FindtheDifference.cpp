4698a305e4a735b35ccaef8933fba804b08151ee
Filip Šrámek <sramek.filip@gmail.com>
2024-04-21 20:01:29 +0200

    Solution

--- FindtheDifference/FindtheDifference.cpp
+++ FindtheDifference/FindtheDifference.cpp
@@ -4,8 +4,17 @@ class Solution
 public:
     char findTheDifference(std::string s, std::string t)
     {
-        int s_sum = static_cast<int>(s);
-        return 'd';
+        int s_sum = 0;
+        int t_sum = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            s_sum += s[i];
+        }
+        for (int i = 0; i < t.size(); i++)
+        {
+            t_sum += t[i];
+        }
+        return t_sum - s_sum;
     }
 };
 
