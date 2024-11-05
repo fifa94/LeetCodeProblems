363dfef506d5f2bd37d2bf87528746c6347009d3
Filip Šrámek <sramek.filip@gmail.com>
2024-11-05 19:57:26 +0100

    Added solution in c#

---
+++ RotateString/RotateStringProj/Program.cs
@@ -0,0 +1,45 @@
+﻿
+public class Solution {
+    public bool RotateString(string s, string goal) {
+
+        if (s == null) {
+            return false;
+        }
+
+        if (s == goal) {
+            return true;
+        }
+
+        List<char> list_s = new List<char>(s.ToCharArray().ToList());
+        List<char> list_goal = new List<char>(goal.ToCharArray().ToList());
+        
+        for (int i = 0; i < list_s.Count; i++) {
+            char first = list_s[0];
+            list_s.RemoveAt(0);
+            list_s.Add(first);
+            if (list_s.SequenceEqual(list_goal)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+
+class SolutionLeetcode
+{
+
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        // Testovací řetězce
+        string s = "abcde";
+        string goal = "cdeab";
+
+        // Volání metody a výstup výsledku
+        bool result = solution.RotateString(s, goal);
+        Console.WriteLine("Výsledek rotace: " + result);  // Očekává true
+
+    }
+}
